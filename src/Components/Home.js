143333f31d09a86482f12bbbe543caad9476a26f
unknown <itsprashant95@bitbucket.org>
2021-06-05 16:49:38 +0530

    ui changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,10 @@ import { Select } from 'antd';
 import track from '../track.mp3';
 import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 
 
@@ -46,6 +50,7 @@ class Home extends Component {
             resListDose2: [],
             errorResp: '',
             cancelBtnShow: 'hide-cancel-btn',
+            showHowToUse: false,
             stateList: [{ "state_id": 1, "state_name": "Andaman and Nicobar Islands" }, { "state_id": 2, "state_name": "Andhra Pradesh" }, { "state_id": 3, "state_name": "Arunachal Pradesh" }, { "state_id": 4, "state_name": "Assam" }, { "state_id": 5, "state_name": "Bihar" }, { "state_id": 6, "state_name": "Chandigarh" }, { "state_id": 7, "state_name": "Chhattisgarh" }, { "state_id": 8, "state_name": "Dadra and Nagar Haveli" }, { "state_id": 37, "state_name": "Daman and Diu" }, { "state_id": 9, "state_name": "Delhi" }, { "state_id": 10, "state_name": "Goa" }, { "state_id": 11, "state_name": "Gujarat" }, { "state_id": 12, "state_name": "Haryana" }, { "state_id": 13, "state_name": "Himachal Pradesh" }, { "state_id": 14, "state_name": "Jammu and Kashmir" }, { "state_id": 15, "state_name": "Jharkhand" }, { "state_id": 16, "state_name": "Karnataka" }, { "state_id": 17, "state_name": "Kerala" }, { "state_id": 18, "state_name": "Ladakh" }, { "state_id": 19, "state_name": "Lakshadweep" }, { "state_id": 20, "state_name": "Madhya Pradesh" }, { "state_id": 21, "state_name": "Maharashtra" }, { "state_id": 22, "state_name": "Manipur" }, { "state_id": 23, "state_name": "Meghalaya" }, { "state_id": 24, "state_name": "Mizoram" }, { "state_id": 25, "state_name": "Nagaland" }, { "state_id": 26, "state_name": "Odisha" }, { "state_id": 27, "state_name": "Puducherry" }, { "state_id": 28, "state_name": "Punjab" }, { "state_id": 29, "state_name": "Rajasthan" }, { "state_id": 30, "state_name": "Sikkim" }, { "state_id": 31, "state_name": "Tamil Nadu" }, { "state_id": 32, "state_name": "Telangana" }, { "state_id": 33, "state_name": "Tripura" }, { "state_id": 34, "state_name": "Uttar Pradesh" }, { "state_id": 35, "state_name": "Uttarakhand" }, { "state_id": 36, "state_name": "West Bengal" }],
         }
     }
@@ -168,10 +173,6 @@ class Home extends Component {
                 disableDistrictDropdown: true,
                 errorResp: '',
                 cancelBtnShow: 'show-cancel-btn',
-
-
-                // show:true,
-
             })
             var self = this
             self.checkSlot(timer)
@@ -195,17 +196,11 @@ class Home extends Component {
     checkSlot = (timer) => {
         console.log('check slot')
 
-        // let formData = new FormData();
-        // formData.append("shop_name", 'test');
-        // https://jsonplaceholder.typicode.com/todos/1
-        // fetch('test'+this.state.district_id + this.state.doseBool, {
         fetch('https://forvaccine.online/go/fetch/' + this.state.district_id + this.state.doseBool, {
             method: 'GET',
-            // body: formData
         }).then((res) => res.json())
             .then(res => {
                 console.log('res =', res)
-
                 var x = []
                 console.log('before map x =', x)
 
@@ -214,55 +209,13 @@ class Home extends Component {
                         if (this.state.dose1 > 0 && i.vaccine == this.state.selectedVaccine && i.min_age_limit == this.state.selectedAgeValue && i.available_capacity > 20 && (i.available_capacity_dose1 > 20)) {
                             x.push({ 's': s, 'sessions': i })
                             console.log('in 1st dose', x)
-
-
-
-
-
                         }
                         else if (this.state.dose2 > 0 && i.vaccine == this.state.selectedVaccine && i.min_age_limit == this.state.selectedAgeValue && i.available_capacity > 20 && (i.available_capacity_dose2 > 20)) {
 
                             console.log('in 2nd dose')
                             x.push({ 's': s, 'sessions': i })
-
-
-
-                            // this.setState({
-
-                            //     showHideClassName: 'modal display-block',
-                            //     show: true,
-                            //     animate_div: '',
-                            //     btn_text: 'Get Notified',
-                            //     resListDose2: res,
-                            //     resListDose1: []
-
-
-                            // }, () => {
-
-                            //     this.beep()
-
-                            // })
-
                         }
-
-                        // else {
-                        //     this.setState({
-
-                        //         showHideClassName: 'modal display-none',
-                        //         show: false,
-                        //         animate_div: 'animated_div',
-                        //         btn_text: 'Finding Slots',
-                        //         resListDose2: [],
-                        //         resListDose1: []
-
-                        //     })
-                        // }
-
-
-
                     })
-
-
                 })
 
                 console.log('after map x =', x)
@@ -271,47 +224,29 @@ class Home extends Component {
 
                     console.log('in x dose 1')
                     this.setState({
-
                         show: true,
-                        // animate_div: '',
-                        // btn_text: 'Get Notified',
                         resListDose1: x,
                         resListDose2: [],
                         showHideClassName: 'modal display-block',
-
-
-
                     }, () => {
-
                         this.beep()
-
                     })
                 }
                 else if (x.length > 0 && this.state.dose2 > 0) {
 
                     console.log('in x dose2')
                     this.setState({
-
                         showHideClassName: 'modal display-block',
                         show: true,
-                        // animate_div: '',
-                        // btn_text: 'Get Notified',
                         resListDose2: x,
                         resListDose1: []
-
-
                     }, () => {
-
                         this.beep()
-
                     })
-
                 }
                 else {
                     console.log('no dose')
-
                     this.setState({
-
                         showHideClassName: 'modal display-none',
                         show: false,
                         animate_div: 'animated_div',
@@ -320,50 +255,12 @@ class Home extends Component {
                         resListDose2: [],
                         resListDose1: [],
                         cancelBtnShow: 'show-cancel-btn',
-
-
                     })
                 }
-                // if (this.state.dose1 > 0 && res.vaccine == this.state.selectedVaccine && res.min_age_limit == this.state.selectedAgeValue && res.available_capacity > 20 && (res.available_capacity_dose1 > 20)){
-
-                //     this.setState({
-
-                //         showHideClassName: 'modal display-block',
-                //         show: true,
-                //         animate_div: '',
-                //         btn_text: 'Get Notified',
-                //         resList: x
-
-                //     }, () => {
-
-                //         this.beep()
-
-                //     })
-
-                // }
-
-
-
-
-                // this.setState({
-
-                //     showHideClassName: 'modal display-block',
-                //     show: true,
-                //     animate_div: '',
-                //     btn_text: 'Get Notified',
-                //     resList: res
-
-                // }, () => {
-
-                //     this.beep()
-
-                // })
             })
             .then(error => {
                 console.log(error)
             })
-
-        // clearInterval(timer)
     }
 
     beep = () => {
@@ -373,8 +270,6 @@ class Home extends Component {
     }
 
     handleCancel = () => {
-
-
 
         this.setState({
             showHideClassName: 'modal display-none',
@@ -387,11 +282,13 @@ class Home extends Component {
             animate_div: 'animated_div',
             btn_text: 'Finding Slots',
             click_btn_disable: true,
+        })
+    }
 
+    handleCancelHowToUse = () => {
 
-
-
-
+        this.setState({
+            showHowToUse: false,
         })
     }
 
@@ -399,12 +296,10 @@ class Home extends Component {
 
         clearInterval(this.state.timer)
         this.setState({
-
             showHideClassName: 'modal display-none',
             show: false,
             animate_div: '',
             click_btn_disable: false,
-
             btn_text: 'Get Notified',
             resListDose2: [],
             resListDose1: [],
@@ -414,26 +309,14 @@ class Home extends Component {
             district_id: '',
             state_id: '',
             district_name: 'Please select district'
-
-
         })
-
-
-
     }
 
     createCardsDose1 = () => {
         console.log('in create cards dose2')
-
         return (
             this.state.resListDose1.map((s, j) => {
-                // console.log('1')
-
                 return (
-                    // s.sessions.map((i, k) => {
-                    // console.log('2')
-
-                    // return (
                     <div>
                         <div className='analytics-card' style={{ textAlign: 'left' }}>
                             <p className='ana-details'>Hospital Name:<span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>{s.s.centerDetails.split('\n')[0].split(':')[1]}</span></p>
@@ -444,8 +327,6 @@ class Home extends Component {
                             <p className='ana-details'>Free/Paid: {s.s.fee_type == 'Paid' ? <><span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>Paid</span></> : <><span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>Free</span></>}</p>
                         </div>
                     </div>
-                    // )
-                    // })
                 )
             })
         )
@@ -454,17 +335,9 @@ class Home extends Component {
 
 
     createCardsDose2 = () => {
-        console.log('in create cards dose2')
-
         return (
             this.state.resListDose2.map((s, j) => {
-                // console.log('1')
-
                 return (
-                    // s.sessions.map((i, k) => {
-                    // console.log('2')
-
-                    // return (
                     <div>
                         <div className='analytics-card' style={{ textAlign: 'left' }}>
                             <p className='ana-details'>Hospital Name:<span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>{s.s.centerDetails.split('\n')[0].split(':')[1]}</span></p>
@@ -475,16 +348,10 @@ class Home extends Component {
                             <p className='ana-details'>Free/Paid: {s.s.fee_type == 'Paid' ? <><span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>Paid</span></> : <><span style={{ float: 'right', fontWeight: '400', color: '#ff6c34' }}>Free</span></>}</p>
                         </div>
                     </div>
-                    // )
-                    // })
                 )
             })
         )
-
     }
-
-
-
 
 
     render() {
@@ -493,14 +360,28 @@ class Home extends Component {
             <div id='wrapper'>
                 <div className='info-section'>
                     <p className='info-section-head'>ForVaccine</p>
-                    <p style={{ fontSize: '16px', padding: '7px' }}>Get notified whenever slots open up. Just fill in the details, sit back and relax!</p>
+                    <p style={{ fontSize: '16px', padding: '7px', marginBottom: '2px' }}>Get notified whenever slots open up. Just fill in the details, sit back and relax!</p>
+                    {/* <div style={{display:'flex', justifyContent:'center'}}>
+                    <p className='details-to-use'><CheckCircleOutlineIcon style={{fontSize:'22px'}}/> Choose details</p>
+                    <p className='details-to-use'><NotificationsNoneIcon style={{fontSize:'22px'}}/> Get notified</p>
+                    <p className='details-to-use'><LocalHospitalIcon style={{fontSize:'22px'}}/> Book slots</p>
+                    </div> */}
+                    <div className='flex-container'>
+                        <p className='details-to-use'><CheckCircleOutlineIcon style={{ fontSize: '22px' }} /> Choose details</p>
+                        <p className='details-to-use'><NotificationsNoneIcon style={{ fontSize: '22px' }} /> Get notified</p>
+                        <p className='details-to-use'><LocalHospitalIcon style={{ fontSize: '22px' }} /> Book slots</p>
+                    </div>
+                    <p style={{ fontSize: '16px', padding: '7px' }}><a className='find_slot'
+                        onClick={() => {
+                            this.setState({
+                                showHowToUse: true
+                            })
+                        }}>How to find a slot?</a></p>
                 </div>
                 <div className='main-box'>
-                    {/* <p className='heading'>GoForVaccine</p> */}
-                    {/* <br></br> */}
                     <div className='form-to-fill'>
-                        <div className='row-1'>
-                            <div className='age-section'>
+                        <div className='row'>
+                            <div className='age-section col'>
                                 <p className='label' style={{ marginTop: '1px' }}>Age</p>
                                 <Radio.Group disabled={this.state.disableOptions} options={this.state.ageOptions} onChange={this.selectAge} value={this.state.selectedAge} />
                             </div>
@@ -587,7 +468,7 @@ class Home extends Component {
 
                     </div>
                 </div>
-                <div><p style={{ fontSize: '16px', color: '#fff', margin: 'auto 1%', padding: '5px', float: 'right' }}>Managed by <a className='creator' href='https://www.linkedin.com/in/abhishek-agarwal-gurugram/'>Abhishek</a> & <a className='creator' href='https://www.linkedin.com/in/itsprashant95/'>Prashant</a></p></div>
+                <div style={{ color: '#fff', textAlign: 'center' }}><p className='footer'>Managed by <a className='creator' href='https://www.linkedin.com/in/abhishek-agarwal-gurugram/'>Abhishek</a> & <a className='creator' href='https://www.linkedin.com/in/itsprashant95/'>Prashant</a></p></div>
 
 
 
@@ -603,6 +484,26 @@ class Home extends Component {
                             {this.state.dose1 > 0 ? <> {this.createCardsDose1()}</> : <>{this.createCardsDose2()}</>}
 
                         </div>
+                    </Modal.Body>
+                </Modal>
+
+                {/* modal for how to use */}
+                <Modal dialogClassName="my-modal" show={this.state.showHowToUse} onHide={this.handleCancelHowToUse}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>It is easy to use</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <ul>
+                            <li>Choose minimum age from given options</li>
+                            <li>Choose number of days</li>
+                            <li>Select state and district</li>
+                            <li>Chooose vaccine</li>
+                            <li>Chooose whether it is your first dose or second</li>
+                            <li>Click on 'Get Notified'</li>
+                        </ul>
+                        <p>Our Webapp will keep on searching for slots according to your choices and notify you. Available slots will be displayed with relevant details.</p>
+                        <p>Best part - if you miss a notification it will still keep on searching for slots until you chose to cancel.</p>
+                        <p></p>
                     </Modal.Body>
                 </Modal>
             </div>
